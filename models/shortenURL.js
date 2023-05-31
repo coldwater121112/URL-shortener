ac13ab2391e407d26903e142f0fff074f76b013e
coldwater121112 <coldwater121112@gmail.com>
2023-05-31 12:39:35 +0800

    feat: add mongoose

---
+++ models/shortenURL.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const shortenURLSchema = new Schema({
+  URL: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  transferURL: {
+    type: String, // 資料型別是字串
+  }
+})
+module.exports = mongoose.model('shortenURL', shortenURLSchema)
