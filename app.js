b5cfe6af509ebd203b703d2c37f51a0fdfaa49b0
coldwater121112 <coldwater121112@gmail.com>
2023-05-31 13:23:29 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // app.js
 const express = require('express')
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose') // 載入 mongoose
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -8,7 +9,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
-mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -21,8 +24,10 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 設定路由
+// 首頁
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
 app.listen(3000, () => {
