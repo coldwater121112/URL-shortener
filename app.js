5d746c03aac7845f6313df937a8f269d04cef6c3
coldwater121112 <coldwater121112@gmail.com>
2023-05-31 19:38:24 +0800

    feat: add shorten.hbs and shorten URL function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 const generateURL = require('./generate_URL')
 const mongoose = require('mongoose') // 載入 mongoose
+const shortenURL = require("./models/shortenURL")
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -25,13 +26,60 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+
 // 設定路由
 // 首頁
-app.get('/', (req, res) => {
-  const transferURL = generateURL()
-  // console.log(transferURL)、
+app.get("/", (req, res) => {
   res.render('index')
 })
+
+app.post("/", (req, res) => {
+  if (!req.body.url) {
+    res.redirect("/");
+  }
+  const originalURL = req.body.url;
+  const transferURL = generateURL();
+  console.log(req.body.url);
+  console.log(req.headers);
+
+  shortenURL.findOne({ URL: originalURL })
+    .lean()
+    .then(url => {
+      if (url) {
+        return url;
+      } else {
+        return shortenURL.create({ URL: originalURL, transferURL: transferURL });
+      }
+    })
+    .then(url => {
+      console.log("AAA", url.transferURL);
+      res.render('shorten', { originalURL: originalURL, transferURL: url.transferURL });
+    })
+    .catch(error => console.error(error));
+});
+
+
+app.get("/:shortURL", (req, res) => {
+  const { shortURL } = req.params
+  console.log(shortURL)
+  shortenURL.findOne({ transferURL: shortURL })
+    .then(url => {
+      if (url) {
+        console.log(url)
+        res.redirect(url.URL)
+      } else {
+        console.log('Short URL not found')
+        res.status(404).send('Short URL not found')
+      }
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(500).send('Internal Server Error')
+    })
+})
+
 
 app.listen(3000, () => {
   console.log('App is runnung on port http://localhost:3000')
--- models/shortenURL.js
+++ models/shortenURL.js
@@ -7,6 +7,7 @@ const shortenURLSchema = new Schema({
   },
   transferURL: {
     type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
   }
 })
 
